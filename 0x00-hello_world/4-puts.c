452ff20a64b8186479e2003490b3acbfbb91d85c
muhammad izge <muhammadmizge8@gmail.com>
2022-12-08 14:58:38 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 
